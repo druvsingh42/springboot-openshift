617628b05f274ce3af36d902dd7924d62405c9b9
druvsingh42 <81846478+druvsingh42@users.noreply.github.com>
2021-08-03 21:45:44 +0530

    Update run-java.sh

--- run-java.sh
+++ run-java.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-java -Djava.net.preferIPv4Stack=true -Xms128m -Xmx256m -jar /app/springboot-kubernetes-v3.jar -b 0.0.0.0 
+java -Djava.net.preferIPv4Stack=true -Xms128m -Xmx256m -jar /app/springboot-kubernetes-v4.jar -b 0.0.0.0 
